0182fe190470ae9e387bf5373432a201143bb9a5
dariahazaparu <61266022+dariahazaparu@users.noreply.github.com>
2023-04-23 02:30:53 +0300

    merg testeeee

--- src/test/java/com/awbd/proiect/services/GenreServiceTest.java
+++ src/test/java/com/awbd/proiect/services/GenreServiceTest.java
@@ -6,8 +6,10 @@ import com.awbd.proiect.repositories.GenreRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Sort;
@@ -19,10 +21,11 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
 public class GenreServiceTest {
 
-    @MockBean
+    @Mock
     GenreRepository genreRepository;
 
     @InjectMocks
@@ -36,10 +39,10 @@ public class GenreServiceTest {
         genre.setName("Romance");
         genresRet.add(genre);
 
-        when(genreRepository.findAll(Sort.by("Name"))).thenReturn(genresRet);
+        when(genreRepository.findAll()).thenReturn(genresRet);
         List<Genre> genres = genreService.findAll();
         assertEquals(genres.size(), 1);
-        verify(genreRepository, times(1)).findAll(Sort.by("Name"));
+        verify(genreRepository, times(1)).findAll();
     }
 
 
